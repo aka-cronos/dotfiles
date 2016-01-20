c4fd37ab0848c6a9eeef26eb51fef7383c515b0f
akacronos <jorge.escamilla89@gmail.com>
2016-01-20 03:33:44 -0600

    Add custom.zsh file for aliases and gitster theme

---
+++ zsh/custom.zsh
@@ -0,0 +1,13 @@
+# Example aliases
+alias zshconfig="source ~/.zshrc"
+alias ohmyzsh="source ~/.oh-my-zsh"
+
+#Custom git aliases
+alias gs="git status"
+alias ga="git add -A"
+alias gco="git checkout"
+alias gb="git branch"
+alias gr="git rebase"
+alias gpsh="git push origin" #push to origin specifying branch, ex. psh master
+alias gpll="git pull origin" #pull from origin specifying branch, ex. pll master
+alias gcm="git commit -m" #commi
