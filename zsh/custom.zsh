5ec116c12fd18c133f454b644f198323c9374b7b
akacronos <jorge.escamilla89@gmail.com>
2016-01-20 04:13:27 -0600

    Add merge alias

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -5,9 +5,10 @@ alias ohmyzsh="source ~/.oh-my-zsh"
 #Custom git aliases
 alias gs="git status"
 alias ga="git add -A"
-alias gco="git checkout"
-alias gb="git branch"
 alias gr="git rebase"
-alias gpsh="git push origin" #push to origin specifying branch, ex. psh master
-alias gpll="git pull origin" #pull from origin specifying branch, ex. pll master
-alias gcm="git commit -m" #commi
+alias gb="git branch"
+alias gm="git merge"
+alias gco="git checkout"
+alias gpsh="git push origin" #push to origin specifying branch, ex. gpsh master
+alias gpll="git pull origin" #pull from origin specifying branch, ex. gpll master
+alias gcm="git commit -m" #commit files
