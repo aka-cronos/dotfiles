4cd100648c9215a811299a4129baf6444c43ec43
akacronos <jorge.escamilla89@gmail.com>
2016-01-21 23:58:29 -0600

    Add alias for heroku push/pull

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -11,4 +11,6 @@ alias gbd="git branch -d" #delete specifying branch, ex. gbd otherBranch
 alias gco="git checkout"
 alias gpsh="git push origin" #push to origin specifying branch, ex. gpsh master
 alias gpll="git pull origin" #pull from origin specifying branch, ex. gpll master
+alias hpsh="git push heroku" #push to origin specifying branch, ex. gpsh master
+alias hpll="git pull heroku" #pull from origin specifying branch, ex. gpll master
 alias gcm="git commit -m" #commit files
