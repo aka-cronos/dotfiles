fcfa465d39cd792b703055eb6cd61336a7a2e372
Jorge Escamilla <jorge.escamilla89@gmail.com>
2017-04-08 15:09:49 -0500

    Add git clone alias

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -1,9 +1,11 @@
 # Example aliases
 alias zshconfig="source ~/.zshrc"
 alias ohmyzsh="source ~/.oh-my-zsh"
+alias subl="subl ."
 
 #Custom git aliases
 alias gs="git status"
+alias gc="git clone"
 alias ga="git add -A"
 alias gr="git rebase"
 alias gb="git branch"
@@ -11,11 +13,12 @@ alias gbd="git branch -d" #delete specifying branch, ex. gbd otherBranch
 alias gco="git checkout"
 alias gpsh="git push origin" #push to origin specifying branch, ex. gpsh master
 alias gpll="git pull origin" #pull from origin specifying branch, ex. gpll master
-alias hpsh="git push heroku" #push to origin specifying branch, ex. gpsh master
-alias hpll="git pull heroku" #pull from origin specifying branch, ex. gpll master
+alias hpsh="git push heroku" #push to origin specifying branch in heroku, ex. gpsh master
+alias hpll="git pull heroku" #pull from origin specifying branch in heroku, ex. gpll master
 alias gcm="git commit -m" #commit files
 alias rs="rails server" #run rails server
 
+#see tree directory levels
 function t() {
   tree -I '.git|node_modules|bower_components|.DS_Store' --dirsfirst --filelimit 20 -L ${1:-3} -aC $2
 }
