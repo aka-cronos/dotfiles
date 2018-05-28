baff1d25dad6f2f188777c757a331c7d896128d3
aka-cronos <jorge.escamilla89@gmail.com>
2018-05-28 10:05:51 -0500

    Remove colors alias

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -1,11 +1,8 @@
-# Example aliases
+#Custom git aliases
 alias zshconfig="source ~/.zshrc"
 alias ohmyzsh="source ~/.oh-my-zsh"
 alias subl="subl ."
-
-alias ls='colorls -r -1'
 alias brewup='brew update; brew upgrade; brew prune; brew cleanup; brew doctor'
-#Custom git aliases
 alias gs="git status"
 alias gc="git clone"
 alias ga="git add -A"
@@ -20,8 +17,3 @@ alias hpsh="git push heroku" #push to origin specifying branch in heroku, ex. gp
 alias hpll="git pull heroku" #pull from origin specifying branch in heroku, ex. gpll master
 alias gcm="git commit -m" #commit files
 alias rs="rails server" #run rails server
-
-#see tree directory levels
-function t() {
-  tree -I '.git|node_modules|bower_components|.DS_Store' --dirsfirst --filelimit 20 -L ${1:-3} -aC $2
-}
