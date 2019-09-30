c63e28ce4b277ecfc9b3cd28ff1fdb015f543191
aka-cronos <jorge.escamilla89@gmail.com>
2019-09-29 22:01:15 -0500

    Update aliases

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -1,6 +1,4 @@
-# Set name of the theme to load. Optionally, if you set this to "random"
-# it'll load a random theme each time that oh-my-zsh is loaded.
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+#iTerm theme
 ZSH_THEME="gitster"
 
 #Custom git aliases
@@ -16,13 +14,13 @@ alias ga="git add -A"
 alias gr="git rebase"
 alias gb="git branch"
 alias gbd="git branch -d" #delete specifying branch, ex. gbd otherBranch
-alias gco="git checkout"
+alias gco="git checkout" #switch in this branch, ex. gco develop
 alias gcob="git checkout -b" #Create the branch on your local machine and switch in this branch
 alias gpsh="git push origin" #push to origin specifying branch, ex. gpsh master
 alias gpll="git pull origin" #pull from origin specifying branch, ex. gpll master
-alias hpsh="git push heroku" #push to origin specifying branch in heroku, ex. gpsh master
-alias hpll="git pull heroku" #pull from origin specifying branch in heroku, ex. gpll master
-alias gcm="git commit -m" #commit files
+alias hpsh="git push heroku" #push to heroku specifying branch, ex. gpsh master
+alias hpll="git pull heroku" #pull from heroku specifying branch, ex. gpll master
+alias gcm="git commit -m" #commit files with message
 alias rs="rails server" #run rails server
-alias startpg="brew services start postgresql"
-alias stoppg="brew services stop postgresql"
+alias startpg="brew services start postgresql" #start postgres
+alias stoppg="brew services stop postgresql" #stop postgres
